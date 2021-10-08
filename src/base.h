84ea60ef141ffbfbd7ab78edf814c89263eab31e
sarah sturkie <sstur002@ucr.edu>
2021-10-07 19:44:06 -0700

    Implemented base.h, added testCommand, added notes

---
+++ src/base.h
@@ -0,0 +1,16 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <string>
+using namespace std;
+
+class Base {
+    public:
+        Base(){}; //constructor
+        virtual void add_lhs(Base*) = 0;
+        virtual void add_rhs(Base*) = 0;
+        virtual bool isEmpty() = 0;
+        virtual bool execute() = 0;
+}
+
+#endif
